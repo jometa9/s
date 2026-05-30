54ae0cb354cbfc920a8fc35bc0fd92b5fda7a653
jometa <joaquinmetayer@gmail.com>
2026-05-30 14:09:39 -0300

    feat: new post 20260608.md

--- src/app/components/HomeLinks.tsx
+++ src/app/components/HomeLinks.tsx
@@ -6,16 +6,15 @@ interface HomeLinksProps {
 
 export default function HomeLinks({ totalPosts = 0 }: HomeLinksProps) {
   return (
-    <div style={{marginBottom: '15px'}}>
-    <Link href="/">
-        <h2 className="homeLinksTitle">jometa</h2>
+    <div style={{ marginBottom: "15px" }}>
+      <Link href="/">
+        <h2 className="homeLinksTitle">joaquin metayer</h2>
       </Link>
-    <p >
-      building <a href="https://www.iptradecopier.com" >@IPTRADE</a>
-    </p>
-      <a href="https://www.linkedin.com/in/joaquinmetayer/" >
-        LinkedIn
-      </a>
-  </div>
+      <p> total posts {totalPosts}</p>
+      <p>
+        <a href="https://github.com/jometa9">GitHub</a>{" "}
+      <a href="https://www.linkedin.com/in/joaquinmetayer/">LinkedIn</a>
+      </p>
+    </div>
   );
 }
